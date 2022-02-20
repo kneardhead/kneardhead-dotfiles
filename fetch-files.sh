f175cbaa1cb95ddf139fbea4e2614e222de7d291
kneardhead <ahmedsabit02@gmail.com>
2022-02-20 14:15:18 -0600

    more files

--- fetch-files.sh
+++ fetch-files.sh
@@ -1,3 +1,5 @@
 cp -rv ~/.config/bspwm ~/kneardhead-dotfiles
 cp -rv ~/.config/sxhkd ~/kneardhead-dotfiles
 cp -rv ~/.config/polybar ~/kneardhead-dotfiles
+cp -rv ~/suckless/dmenu ~/kneardhead-dotfiles
+cp -rv ~/suckless/st ~/kneardhead-dotfiles
