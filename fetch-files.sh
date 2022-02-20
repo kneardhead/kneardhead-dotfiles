5e39b745c2a17d3f19cd067b0d76acb2fad0e334
kneardhead <ahmedsabit02@gmail.com>
2022-02-20 14:44:19 -0600

    fetch files

--- fetch-files.sh
+++ fetch-files.sh
@@ -3,3 +3,5 @@ cp -rv ~/.config/sxhkd ~/kneardhead-dotfiles
 cp -rv ~/.config/polybar ~/kneardhead-dotfiles
 cp -rv ~/suckless/dmenu ~/kneardhead-dotfiles
 cp -rv ~/suckless/st ~/kneardhead-dotfiles
+cp -rv ~/.config/ranger ~/kneardhead-dotfiles/
+cp -rv ~/suckless/Zathura-Pywal ~/kneardhead-dotfiles/
