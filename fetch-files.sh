37888a06c81aac812e132056d1618cdc9df55eff
kneardhead <ahmedsabit02@gmail.com>
2022-02-20 23:01:35 -0600

    more files

--- fetch-files.sh
+++ fetch-files.sh
@@ -4,4 +4,5 @@ cp -rv ~/.config/polybar ~/kneardhead-dotfiles
 cp -rv ~/suckless/dmenu ~/kneardhead-dotfiles
 cp -rv ~/suckless/st ~/kneardhead-dotfiles
 cp -rv ~/.config/ranger ~/kneardhead-dotfiles/
-cp -rv ~/suckless/Zathura-Pywal ~/kneardhead-dotfiles/
+cp -rv ~/suckless/zathura-pywal ~/kneardhead-dotfiles/
+cp -rv /usr/share/fonts ~/kneardhead-dotfiles/
